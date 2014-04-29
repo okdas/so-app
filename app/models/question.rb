fceba65258a683adcebd33dd18663db0fabbf254
Dmitry Knyazev <me@kdas.ru>
2014-04-29 16:59:16 +0400

    Question model validations with tests.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,15 @@
 class Question < ActiveRecord::Base
+
+  validates_presence_of :title, :body
+
+  validates_uniqueness_of :title, message: 'This question is already exists.'
+
+  validates_length_of :title, in: 10..40,
+                      too_short: 'Title must be more than 10 characters',
+                      too_long: 'Title cannot be more than 40 characters'
+
+  validates_length_of :body, in: 50..5000,
+                      too_short: 'Question must be more than 50 characters',
+                      too_long: 'Question cannot be more than 5000 characters'
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+
+  # TODO: uniqueness tests with a factory girl
+  it { should validate_uniqueness_of(:title).with_message('This question is already exists.') }
+
+  it do
+    should ensure_length_of(:title).
+               is_at_least(10).
+               is_at_most(40).
+               with_short_message('Title must be more than 10 characters').
+               with_long_message('Title cannot be more than 40 characters')
+  end
+
+  it do
+    should ensure_length_of(:body).
+               is_at_least(50).
+               is_at_most(5000).
+               with_short_message('Question must be more than 50 characters').
+               with_long_message('Question cannot be more than 5000 characters')
+  end
+
+
 end
