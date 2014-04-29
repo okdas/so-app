5f0e9b8f703fd72d86388d48c71b72fc96ebcf09
Dmitry Knyazev <me@kdas.ru>
2014-04-29 22:30:05 +0400

    User name validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
+
+  validates_length_of :name, in: 2..20,
+                      too_short: 'Name must be more than 2 characters',
+                      too_long: 'Name cannot be more than 20 characters'
+
   has_many :questions
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe User do
 
+  it { should validate_presence_of :name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  it { should have_many(:questions) }
+  it { should have_many :questions }
+
+  it do
+    should ensure_length_of(:name).
+               is_at_least(2).
+               is_at_most(20).
+               with_short_message('Name must be more than 2 characters').
+               with_long_message('Name cannot be more than 20 characters')
+  end
+
 
 end
