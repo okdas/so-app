34c5dfc889e20a54c38508559fc1a16ae498759f
Dmitry Knyazev <me@kdas.ru>
2014-06-05 00:04:11 +0400

    Improve specs.

--- spec/acceptance/votes/question_votes_spec.rb
+++ spec/acceptance/votes/question_votes_spec.rb
@@ -1,4 +1,4 @@
-include 'acceptance/acceptance_helper'
+require 'acceptance/acceptance_helper'
 
 feature 'Vote for question', %q{
   As an aunthenticated user
@@ -6,15 +6,41 @@ feature 'Vote for question', %q{
 } do
   given(:user) { create :user }
   given(:question) { create :static_question }
-  scenario 'Authenticated user trying to upvote question' do
+  scenario 'Authenticated user trying to upvote question', js: true do
+    login_from_form(user)
     visit question_path question
 
-    click_on '.upvote_question'
+    within '.question .votecount' do
+      expect(page).to have_content '0'
+    end
+
+    click_on 'Vote up'
 
     within '.question .votecount' do
       expect(page).to have_content '1'
     end
   end
-  scenario 'Not authenticated user trying to vote question'
-  scenario 'Authenticated user trying to downvote question'
+  scenario 'Not authenticated user trying to vote question' do
+    visit question_path question
+
+    within '.question .vote' do
+      expect(page).not_to have_content 'Vote down'
+      expect(page).not_to have_content 'Vote up'
+    end
+  end
+
+  scenario 'Authenticated user trying to downvote question', js: true do
+    login_from_form(user)
+    visit question_path question
+
+    within '.question .votecount' do
+      expect(page).to have_content '0'
+    end
+
+    click_on 'Vote down'
+
+    within '.question .votecount' do
+      expect(page).to have_content '-1'
+    end
+  end
 end
