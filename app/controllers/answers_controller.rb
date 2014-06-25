b9ef92841a4af16e06292ac6dd39b63db4c26ac5
Dmitry Knyazev <me@kdas.ru>
2014-06-25 17:10:02 +0400

    IR: Answers.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,42 +1,24 @@
-class AnswersController < ApplicationController
+class AnswersController < InheritedResources::Base
   before_action :authenticate_user!, only: [:create, :edit]
-  before_action :load_answer, only: [:edit, :update]
-  before_action :answer_belongs_to_current_user, only: [:edit, :update]
+
+  respond_to :js
+  actions :create, :update, :edit
+
+  belongs_to :question
 
   load_and_authorize_resource
 
-  def create
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.build(answer_params)
-    @answer.user = current_user
-    @answer.attachments.each do |attachment|
+  protected
+
+  def create_resource(object)
+    object.user = current_user
+    object.attachments.each do |attachment|
       attachment.user = current_user
     end
-    @answer.save
-    # redirect_to question_path(@answer.question)
-  end
-
-  def edit
-  end
-
-  def update
-    @answer.update(answer_params)
-  end
-
-  private
-
-  def load_answer
-    @answer = Answer.find(params[:id])
+    super
   end
 
   def answer_params
     params.require(:answer).permit(:body, attachments_attributes: [:id, :_destroy, :attachment])
-  end
-
-  def answer_belongs_to_current_user
-    unless current_user == @answer.user
-      flash[:notice] = 'This is not your answer.'
-      redirect_to root_path
-    end
   end
 end
